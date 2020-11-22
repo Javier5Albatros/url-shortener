50c4eddf2e41c0d80042460fbb8d4aa96b9bb49e
Abortos <javieralvarez2.ja@gmail.com>
2020-11-22 18:12:11 +0100

    Refactor with pydantic classes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel, ValidationError, validator
 from starlette.responses import RedirectResponse
 import hashlib
 import redis
@@ -19,26 +20,51 @@ url_regex = re.compile(
     r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
 
+class Url(BaseModel):
+    url: str
+    url_hash: str
+
+    @validator('url', pre=True)
+    def is_url(cls, v):
+        if(not re.match(url_regex, v)):
+            raise ValueError('Malformed URL: '+v)
+        return v
+
+
 @app.put("/url")
 async def save_url(url: str):
-    if(not re.match(url_regex, url)):
-        return {"status": 0, "message": "Malformed url"}
-    else:
-        url_hashed = shortuuid.uuid(name=url, pad_length=10)
-        if(r.get("url:" + url_hashed)):
-            return {"status": 0, "message": "Url already registered"}
-        else:
-            r.set("url:" + url_hashed, url)
-            return {"status": 1, "message": "Url succesfully registered", "url": url_hashed}
+    try:
+        url = Url(
+            url=url,
+            url_hash=shortuuid.uuid(name=url, pad_length=10)
+        )
+        r.set("url:" + url.url_hash, url.url)
+        return {"status": 1, "message": "Url succesfully registered", "url": url.url_hash}
+    except ValidationError as e:
+        return {"status": 0, "error": str(e)}
 
 
 @app.get("/urls")
 async def get_urls():
-    urls = r.keys("*url:*")
-    if(urls):
-        return {"status": 1, "urls": urls}
-    else:
-        return {"status": 0, "message": "Error fetching the urls"}
+    urls = {}
+    keys = r.keys("*url:*")
+    if(not keys):
+        return {'status': 0, "message": "There are no urls"}
+
+    for i, key in enumerate(keys):
+        key = key.decode("utf-8")
+        value = r.get(key)
+        value = value.decode("utf-8")
+        url_hash = key[4:]
+        try:
+            url = Url(
+                url=value,
+                url_hash=url_hash
+            )
+            urls[i] = url.dict()
+        except ValidationError as e:
+            return {"status": 0, "error": str(e)}
+    return urls
 
 
 @app.get("/{url_hash}")
